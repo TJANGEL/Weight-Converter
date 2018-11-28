4bca279177bd2d139107d5ae9f064e9cef43f3ca
Dante <42275387+TJANGEL@users.noreply.github.com>
2018-11-28 11:48:00 -0500

    added js file

---
+++ assets/converter.js
@@ -0,0 +1,17 @@
+document.getElementById('output').style.visibility = 'hidden';
+
+document.getElementById('lbsInput').addEventListener('input', function () {
+    
+    document.getElementById('output').style.visibility = 'visible';
+
+    let lbs = e.target.value;
+    
+    // convert to grams
+    document.getElementById('gramsOutput').innerHTML = lbs/0.0022046;
+
+    // convert to kilograms
+    document.getElementById('kgOutput').innerHTML = lbs/2.2046;
+
+    // convert to ounces
+    document.getElementById('ozOutput').innerHTML = lbs*16;
+})
